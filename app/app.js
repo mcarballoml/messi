a23e906748756fb84d3a52d52f525719f2197cdc
Matias Carballo <mcarballo@fi.uba.ar>
2026-05-28 01:28:11 -0300

    Flags highlight

--- app/app.js
+++ app/app.js
@@ -3,11 +3,16 @@ import { loadData } from './data.service.js';
 import { renderGroups, renderCalendar } from './render.js';
 
 window.selectTeam = (team) => {
-  state.selectedTeam =
-    state.selectedTeam === team ? null : team;
+  const isSame = state.selectedTeam === team;
+  state.selectedTeam = isSame ? null : team;
+
+  if (!isSame) {
+    const group = state.teamToGroup[team];
+    state.openGroups.add(group);
+  }
+
   renderGroups(state);
 };
-
 async function init() {
 
   const data = await loadData();
@@ -15,6 +20,14 @@ async function init() {
   state.groups = data.groups;
   state.matches = data.matches;
   state.openGroups = new Set();
+
+  state.teamToGroup = {};
+  for (const group in state.groups) {
+    const teams = state.groups[group];
+    for (const team of teams) {
+      state.teamToGroup[team] = group;
+    }
+  }
 
   renderGroups(state);
   renderCalendar(state);
